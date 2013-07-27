32c91488b66d0117cdfaa146900b4d242eb6095c
Jack Lin <jacklinmaker@gmail.com>
2013-07-27 17:10:38 +0800

    add base_title instace variable in spec controller helper

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,11 @@ require 'spec_helper'
 #render_views
 
 describe PagesController do
+  render_views
+
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial"
+  end
 
   describe "GET 'home'" do
 
@@ -13,7 +18,7 @@ describe PagesController do
 =begin
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title",:content => "Ruby on Rails Tutorial Sample App | Home")
+      response.should have_selector("title",:content => @base_title + " | Home")
     end
 =end
   end
@@ -26,7 +31,7 @@ describe PagesController do
 =begin
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title",:content => "Ruby on Rails Tutorial Sample App | Contact")
+      response.should have_selector("title",:content => @base_title " | Contact")
     end
 =end
   end
@@ -39,7 +44,7 @@ describe PagesController do
 =begin
     it "should have the right title" do
       get 'help'
-      response.should have_selector("title",:content => "Ruby on Rails Tutorial Sample App | Help")
+      response.should have_selector("title",:content => @base_title + " | Help")
     end
 =end
   end
